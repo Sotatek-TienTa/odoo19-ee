3216732e82f3cb137a4fba9d8165d134d70e5c6b
Sotatek-TienTa <tien.ta@sotatek.com>
2026-01-14 14:27:30 +0700

    git init

---
+++ odoo/addons/l10n_ch_hr_payroll/tests/__init__.py
@@ -0,0 +1,6 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from . import test_contract_template_whitelist
+from . import swissdec_common
+from . import test_swissdec_cases
+from . import test_swiss_warnings
